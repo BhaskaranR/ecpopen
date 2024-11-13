7cfa47dde16239c1ca31bd0e68f464a3efcf72be
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2024-11-12 19:44:06 -0800

    minor fix for jenkins build

---
+++ vite.config.cjs
@@ -0,0 +1,24 @@
+const path = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/js/index.js'),
+      name: 'ECPOpen',
+      fileName: () => 'ecpopen.js',
+      formats: ['iife'],
+    },
+    rollupOptions: {
+      output: {
+        extend: true,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'ecpopen.css';
+          }
+          return `assets/[name][extname]`;
+        },
+      }
+    },
+  }
+}); 
